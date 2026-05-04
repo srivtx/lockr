55b720b2e571fe98ea376d60ef1908c4af55fe73
srivtx <crypticcc101@gmail.com>
2026-05-05 02:15:40 +0530

    fix: cast Solana wallet adapter components to fix React 18.3 types mismatch

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import React, { useMemo } from "react";
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  ConnectionProvider as _ConnectionProvider,
+  WalletProvider as _WalletProvider,
+} from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider as _WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
+
+// Cast to work around @types/react 18.3 JSX component type mismatch
+const ConnectionProvider = _ConnectionProvider as React.ComponentType<any>;
+const WalletProvider = _WalletProvider as React.ComponentType<any>;
+const WalletModalProvider = _WalletModalProvider as React.ComponentType<any>;
 
 export default function LockrWalletProvider({
   children,
